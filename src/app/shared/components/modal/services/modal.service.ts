e73f6ef6f8ea0257bfa90482acc97c079f6ad60e
Caitano <josiasmartinscaitano@gmail.com>
2022-08-23 08:28:02 -0300

    feat: definindo a interface da nossa API

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, TemplateRef } from '@angular/core';
 
 @Injectable()
-export class ModalService {}
+export class ModalService {
+
+  // templateRef: Referencia do template
+  // templateRef: refencia do conteudo que precisa estar dentro do modal
+  public open(config: ModalConfigInterface) {
+    console.log('open called');
+    return new ModalRef()
+  }
+}
+
+export interface ModalConfigInterface {
+  templateRef: TemplateRef<any>;
+  title: string;
+}
+
+
+export class ModalRef {
+
+  public close(): void {
+    console.log('close called')
+  }
+}

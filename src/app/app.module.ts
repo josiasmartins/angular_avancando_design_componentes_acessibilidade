25da2349fd4c5aad777329ffeb50bb6b79964385
Caitano <josiasmartinscaitano@gmail.com>
2022-08-23 07:59:27 -0300

    feat: projeto inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ModalModule } from './shared/components/modal/modal.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ModalModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/components/modal/modal.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ModalComponent } from './modal.component';
+import { ModalService } from './services/modal.service';
+
+@NgModule({
+  declarations: [ModalComponent],
+  imports: [CommonModule],
+  exports: [ModalComponent],
+  providers: [ModalService]
+})
+export class ModalModule {}

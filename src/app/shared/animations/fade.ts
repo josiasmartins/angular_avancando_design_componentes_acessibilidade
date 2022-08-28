318c54809723af2116d02b6d0e1e94159aff4373
Caitano <josiasmartinscaitano@gmail.com>
2022-08-28 15:49:19 -0300

    feat: criado o animation fadeIn

---
+++ src/app/shared/animations/fade.ts
@@ -0,0 +1,14 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+
+export const fade = trigger(
+  'fade', 
+  [
+    transition(
+      ':enter',
+      [ 
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 }))
+      ]
+    )
+  ]
+);
--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,13 +1,15 @@
 import { AfterViewInit, Component, HostBinding, TemplateRef } from '@angular/core';
 import { ModalConfigInterface } from './interfaces/modal-config';
+import { fade } from '../../animations/fade';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [fade]
 })
 export class ModalComponent implements AfterViewInit {
-
+  @HostBinding('@fade') fade = true;
   // @HostBinding('class.show') public show = false;
   public modalConfigI: ModalConfigInterface;
 

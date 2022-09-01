43ff5776d671df3fad808367fc023188d9ea1569
Caitano <josiasmartinscaitano@gmail.com>
2022-09-01 08:08:57 -0300

    feat: adicionado a descrição sobre o document.activeElement

--- src/app/shared/directives/focus-back/focus-back.directive.ts
+++ src/app/shared/directives/focus-back/focus-back.directive.ts
@@ -8,6 +8,11 @@ export class FocusBackDirective implements OnInit, OnDestroy {
   private lastFocusedElement: Element;
   
   public ngOnInit(): void {
+    /**
+     *  document.activeElement
+     * @description
+     *  Nos dá uma referência para o elemento com focus dentro do documento (página) exibida.
+     */
     this.lastFocusedElement = document.activeElement;
   }
 
